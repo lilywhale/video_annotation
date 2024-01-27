8dd39a38c3c8215a7f6d20dd1e37cb79f6194a71
lilywhale <94987363+lilywhale@users.noreply.github.com>
2024-01-27 19:12:00 +0100

    merge annotation form and youtube viewer 1

--- static/js/script.js
+++ static/js/script.js
@@ -46,3 +46,46 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Additional controls can be added based on your requirements
 });
+
+var min = 0;
+var max_interval = 1000;
+var step = 0.1;
+var from = 0;
+var to = 60;
+var videotime = 0;
+
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// This function creates an <iframe> (and YouTube player)
+// after the API code downloads.
+var player;
+
+
+/****************************************/
+// (Modification) 
+// Fonction pour extraire les paramètres de l'URL
+function getQueryParam(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+var videoId = getQueryParam('videoId') || 'WNCl-69POro'; // ID par défaut si aucun paramètre n'est trouvé
+
+// Fonction pour mettre à jour les paramètres de l'URL
+function updateUrlParameter(url, paramName, paramValue) {
+    var pattern = new RegExp('('+paramName+'=).*?(&|$)'),
+        newUrl = url.replace(pattern, '$1' + paramValue + '$2');
+    if (!url.match(pattern)) {
+        newUrl = newUrl + (newUrl.indexOf('?')>0 ? '&' : '?') + paramName + '=' + paramValue;
+    }
+    return newUrl;
+}
